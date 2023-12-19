e0d69c70bf31031d5a449c7ae05729f02731112b
Joel <kjoelpaul94@gmail.com>
2023-12-19 00:31:52 +0000

    Enhance: Added comments and accessibility enhancements via ARIA labels

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function hideProjectInformationPanel() {
     var projectInfoPanel = document.querySelectorAll('.project-information-panel');
-    if (projectInfoPanels.length > 0) {
-        projectInfoPanels[0].style.display = "none";
+    if (projectInfoPanel.length > 0) {
+        projectInfoPanel[0].style.display = "none";
     }
 
     var projectInfoPanelContainer = document.querySelectorAll('.project-information-panel-container');
